607fb3860f34508308335b506b3b4357b3b751f7
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-04 01:53:29 +0600

    Work with REST API from React App using (axios)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React,{ Component } from "react";
 import './App.css';
-import  fetch from "cross-fetch";
+import  axios from "axios";
 
 class App extends Component{
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.json())
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response=>response.data)
     .then(data=>console.log(data))
 
   }
