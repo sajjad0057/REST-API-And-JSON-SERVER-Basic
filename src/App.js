cae30089aed664f607751ab2e2d7aefe1ba8b6dd
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-04 04:35:59 +0600

    Handling REST API  Responses Using State

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import  axios from "axios";
 
 class App extends Component{
+
+  state = {
+    dishes : null,
+    errMesss : null, 
+   }
+  
   componentDidMount(){
-    // axios.get('http://localhost:3001/dishes/')    // axios.get() function performing GET request for get data
-    // .then(response=>response.data)
-    // .then(data=>console.log(data))
-    // .catch(error=>console.log(error.message))
+    console.log("componentDidMount--->",this.state);
+    axios.get('http://localhost:3001/dishes/7')              // axios.get() function performing GET request for get data
+    .then(response=>response.data)
+    .then(data=>{
+      this.setState({
+        dishes:data
+      })
+    })
+    .catch(error=>{
+      this.setState({
+        errMesss : error.message
+      })
+    })
 
     // axios.post('http://localhost:3001/dishes/',{name:"pizza",price: "449"})     //axios.post() function performing POST request for submit data to server.
     // .then(response=>console.log(response))
@@ -15,10 +30,15 @@ class App extends Component{
     // axios.put('http://localhost:3001/dishes/2',{name: "cheese cake", price:"99"})      // axios.put() function performing PUT request for change or update data to server.
     // .then(request=>console.log(request))
 
-    axios.delete('http://localhost:3001/dishes/3')      // axios.delete() function performing DELETE request to delete data from server.
-    .then(request=>console.log(request))
+    // axios.delete('http://localhost:3001/dishes/3')      // axios.delete() function performing DELETE request to delete data from server.
+    // .then(request=>console.log(request))
 
   }
+
+  componentDidUpdate(){
+    console.log("componentDidUpdate---->",this.state);
+  }
+
   render(){
     return(
       <div>
