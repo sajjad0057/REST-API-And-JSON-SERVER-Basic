5f9a03c07ee190a4a816650c92ba1ebcaa43be72
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-04 03:51:40 +0600

    Error Handling, POST, PUT, DELETE performing with axios

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import  axios from "axios";
 
 class App extends Component{
   componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(response=>response.data)
-    .then(data=>console.log(data))
+    // axios.get('http://localhost:3001/dishes/')    // axios.get() function performing GET request for get data
+    // .then(response=>response.data)
+    // .then(data=>console.log(data))
+    // .catch(error=>console.log(error.message))
+
+    // axios.post('http://localhost:3001/dishes/',{name:"pizza",price: "449"})     //axios.post() function performing POST request for submit data to server.
+    // .then(response=>console.log(response))
+
+    // axios.put('http://localhost:3001/dishes/2',{name: "cheese cake", price:"99"})      // axios.put() function performing PUT request for change or update data to server.
+    // .then(request=>console.log(request))
+
+    axios.delete('http://localhost:3001/dishes/3')      // axios.delete() function performing DELETE request to delete data from server.
+    .then(request=>console.log(request))
 
   }
   render(){
